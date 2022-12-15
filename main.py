5d283e736a01a30f4b663ee928c2de465489309c
Jose-V12 <jv123144@gmail.com>
2022-12-15 12:30:00 -0600

    created Database named Hospital

--- main.py
+++ main.py
@@ -13,5 +13,13 @@ def create_server_connection(hose_name, user_name, user_password):
     except Error as err:
         print(f"Error: '{err}'")
     return connection
-
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as err:
+        print(f"Error as '{err}'")
 connection = create_server_connection("localhost", "root", "student")
+create_database_query = "CREATE DATABASE Hospital"
+create_database(connection, create_database_query)
