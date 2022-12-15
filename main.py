fb2cbb4e45558990a99c418162ca4b419bf0a815
Jose-V12 <jv123144@gmail.com>
2022-12-15 12:24:40 -0600

    imported libraries and connected pycharm to mysql server

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import mysql.connector
 from mysql.connector import Error
+
+def create_server_connection(hose_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = hose_name,
+            user = user_name,
+            passwd = user_password
+        )
+        print("MYSQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+    return connection
